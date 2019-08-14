f06e57dc8e418bf127cd135377fdd336e2f1905e
Mike JS. Choi <mkchoi212@icloud.com>
2019-08-14 07:02:47 -0700

    Initial Commit

---
+++ Basil/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Basil
+//
+//  Created by Mike Choi on 8/14/19.
+//  Copyright © 2019 435. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
